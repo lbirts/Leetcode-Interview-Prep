f1f9f51c18a2728b3667b1e40be21169a0463689
Lauren Birts <59886502+lbirts@users.noreply.github.com>
2020-10-21 12:08:26 -0500

    Update removeDuplicates.js

--- arrays/removeDuplicates.js
+++ arrays/removeDuplicates.js
@@ -1,4 +1,21 @@
 
 var removeDuplicates = function(nums) {
-   return nums.filter((item,index) => nums.indexOf(item) === index)
+    // if (nums.length === 0) return nums
+    for(let i = 1; i < nums.length; i++){
+     if(nums[i] === nums[i-1]){
+        nums.splice(i, 1)
+         --i
+     }
+   }
+    
+    // let i=0;
+    // while(i<nums.length-1){
+    //     let j=i+1;
+    //     if(nums[i]===nums[j]){
+    //         nums.splice(j,1);
+    //     }
+    //     else{
+    //         i++;
+    //     }
+    // }
 };
