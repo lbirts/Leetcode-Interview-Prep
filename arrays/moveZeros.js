1ea519d468c78b7c27b358962f52e7a755bcf447
Lauren Birts <59886502+lbirts@users.noreply.github.com>
2020-10-21 13:08:30 -0500

    Create moveZeros.js

---
+++ arrays/moveZeros.js
@@ -0,0 +1,9 @@
+var moveZeroes = function(nums) {
+    for (let i = nums.length; i>=0; i--) {
+        if (nums[i] === 0) {
+            nums.push(0)
+            nums.splice(i, 1)
+        }
+    }
+    
+};
