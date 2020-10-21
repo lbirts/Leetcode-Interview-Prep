59f69e3659e36b8cf40ecee56967960f8d64f940
Lauren Birts <59886502+lbirts@users.noreply.github.com>
2020-10-21 12:14:05 -0500

    Create containsDuplicates.js

---
+++ arrays/containsDuplicates.js
@@ -0,0 +1,12 @@
+var containsDuplicate = function(nums) {
+    let matching = []
+    for (let i = 0; i < nums.length; i++) {
+        if (matching.includes(nums[i])) {
+            return true
+        } else {
+            matching.push(nums[i])
+        }
+    }
+    return false
+    
+};
