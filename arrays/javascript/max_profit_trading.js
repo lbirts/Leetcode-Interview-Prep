d7e3b8b0a0dddb17ff0c295772dd1101bdccde38
Lauren Birts <59886502+lbirts@users.noreply.github.com>
2020-11-20 20:43:31 -0600

    Create max_profit_trading.js

---
+++ arrays/javascript/max_profit_trading.js
@@ -0,0 +1,9 @@
+var maxProfit = function(prices) {
+    profit = 0
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i+1] && prices[i] < prices[i+1]) {
+            profit+=prices[i+1] - prices[i]
+        }
+    }
+    return profit
+};
