88b3f5ac0fc6160493b2d98bd31d248ff16ad2c7
Lauren Birts <59886502+lbirts@users.noreply.github.com>
2020-11-23 17:31:29 -0600

    Update rotate2dArray.rb

--- arrays/ruby/rotate2dArray.rb
+++ arrays/ruby/rotate2dArray.rb
@@ -1,3 +1,11 @@
 def rotate(matrix)
-     matrix.replace(matrix.reverse.transpose)
+    duplicate = Marshal.load(Marshal.dump(matrix))
+    duplicate.each_with_index do |row, row_index|
+        reverse = matrix.length
+        row.each_with_index do |column, col_index|
+            reverse-=1
+            matrix[row_index][col_index] = duplicate[reverse][row_index]
+        end
+    end
+    # matrix.replace(matrix.reverse.transpose)
 end
